2de2c878c82bb26c0b4b35ece5c44a97cf708d93
Okba CEMUZ <=>
2022-12-15 00:58:07 +0300

    commit

--- src/api/v1/auth/auth.schema.ts
+++ src/api/v1/auth/auth.schema.ts
@@ -3,7 +3,7 @@ import Joi from 'joi';
 class AuthSchema {
     public login(): Joi.ObjectSchema {
         return Joi.object({
-            email: Joi.string().max(20).required(),
+            email: Joi.string().max(100).required(),
             password: Joi.string().min(6).max(20).required(),
         });
     }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import dotenv from "dotenv";
-import http from "http";
-import AuthController from "./api/v1/auth/auth.controller";
-import App from "./app";
+import dotenv from 'dotenv';
+import http from 'http';
+import AuthController from './api/v1/auth/auth.controller';
+import App from './app';
 
 // load environment variables
 dotenv.config();
@@ -9,17 +9,24 @@ dotenv.config();
 // port from .env
 const port = process.env.PORT;
 
-// set app port 
-const app  = new App(
-    [
-        new AuthController()
-    ]
-);
+// set app port
+const app = new App([new AuthController()]);
 
 // create http server from express app
 const server = http.createServer(app.getApp());
 
 // server listen on ${process.env.PORT}
-server.listen(port,()=> {
+server.listen(port, () => {
     console.log(`server is up on http://localhost:${port}`);
-})
+});
+
+server.on('request', () => {
+    const arr = [1, 2, 3, 4, 5, 6, 9, 7, 8, 9, 10];
+    arr.reverse();
+    const used: any = process.memoryUsage();
+    for (let key in used) {
+        console.log(
+            `${key} ${Math.round((used[key] / 1024 / 1024) * 100) / 100} MB`
+        );
+    }
+});
